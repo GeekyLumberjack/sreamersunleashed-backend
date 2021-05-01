5dd8616e500f4c2ccab8da515357f18cd5d72bf7
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-01 14:57:20 -0500

    readding CORS header

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -25,7 +25,7 @@ export default function handler(lambda) {
       body: JSON.stringify(body),
       headers: {
         "Access-Control-Allow-Origin": "*",
-        
+        "Access-Control-Allow-Credentials": true,
       },
     };
   };
