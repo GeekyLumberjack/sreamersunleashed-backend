b743f7d06642833176e924d2b29ed67b145e3c13
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-05 23:22:19 -0500

    Adding walletAddress to params

--- streamlabsAccess.js
+++ streamlabsAccess.js
@@ -4,7 +4,8 @@ import handler from "./libs/handler-lib";
 export const main = handler( async (event, context) => {
   const data = JSON.parse(event.body);
   var params ={TableName: `customerTable`,
-    Item:{code:data.code}
+    Item:{walletAddress:data.walletAddress,
+      code:data.code}
   };
 
   try {
