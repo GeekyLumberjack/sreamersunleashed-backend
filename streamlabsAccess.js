adbb3c189d03d10e1a8ac8aff9bff6d41727e2ca
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-12 23:12:22 -0500

    adding donate endpoint

--- streamlabsAccess.js
+++ streamlabsAccess.js
@@ -1,45 +1,16 @@
 import dynamoDb from "./libs/dynamodb-lib";
 import handler from "./libs/handler-lib";
 
+var AWS = require('aws-sdk');
+  const ssm = new AWS.SSM();
+  const clientSecretPromise = ssm.getParameter({
+    Name: 'client_secret',
+    WithDecryption: true
+  }).promise();
 
 async function getAccessToken(code) {
-  var AWS = require('aws-sdk');
-  var region = "us-east-2";
-  var secretName = "client_secret";
-  var secret;
-  var client = new AWS.SecretsManager({
-    region: region
-  });
-  client.getSecretValue({SecretId: secretName}, function(err, data) {
-    if (err) {
-        if (err.code === 'DecryptionFailureException')
-            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InternalServiceErrorException')
-            // An error occurred on the server side.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InvalidParameterException')
-            // You provided an invalid value for a parameter.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'InvalidRequestException')
-            // You provided a parameter value that is not valid for the current state of the resource.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-        else if (err.code === 'ResourceNotFoundException')
-            // We can't find the resource that you asked for.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-    }
-    else {
-        // Decrypts secret using the associated KMS CMK.
-        // Depending on whether the secret is a string or binary, one of these fields will be populated.
-        secret = data.SecretString;
-    }
-  });
   var axios = require("axios");
+  const secret = await clientSecretPromise;
   const res = await axios.post('https://streamlabs.com/api/v1.0/token?grant_type=authorization_code&client_id=OTgPYBs7dsnSJN6yph3HlYDpjCyEx4q5lXyLskds&client_secret='+secret+'&redirect_uri=streamersunleashed.com&code='+code);
   console.log(res);
   return{body:JSON.stringify(res)};
