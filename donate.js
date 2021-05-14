d84a1dfe925f66e8fdb92baa4d5cdae0d2da0161
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-14 12:25:05 -0500

    adding donate endpoint

--- donate.js
+++ donate.js
@@ -3,7 +3,7 @@ import handler from "./libs/handler-lib";
 
 async function sendDonation(data, hasCode) {
       var axios = require("axios");
-      const res = await axios.post('https://streamlabs.com/api/v1.0/donations?name='+data.name+'&identifier='+data.name+'&amount='+data.amount+'&currency='+data.currency+'&access_token='+hasCode.access_token);
+      const res = await axios.post('https://streamlabs.com/api/v1.0/donations?name='+data.name+'&identifier='+data.name+'&amount='+data.amount+'&currency='+data.currency+'&access_token='+hasCode.Item.access_token);
       console.log(res);
       return{body:JSON.stringify(res)};
     }
@@ -12,15 +12,16 @@ export const main = handler( async (event, context) => {
   const data = JSON.parse(event.body);
   var params = {
     TableName: `customerTable`,
-    FilterExpression: "contains(tokenMap,:wa)",
+    KeyConditionExpression: "walletAddress = :wa",
     ExpressionAttributeValues:{
-      ":wa" : {"S": data.token}
+      ":wa" : data.walletAddress
     },
-    Select:"access_token"
+    ProjectionExpression:"access_token"
 
   };
   try{
-    const hasCode = await dynamoDb.scan(params);
+    const hasCode = await dynamoDb.get(params);
+    console.log(hasCode);
     const sendIt = await sendDonation(data, hasCode);
     return ({Donation: true, Response: sendIt});
   } catch (e) {
