ff39a058d723a68216251d802f0ee6aac31abb91
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-14 01:03:34 -0500

    adding donate endpoint

--- donate.js
+++ donate.js
@@ -3,7 +3,7 @@ import handler from "./libs/handler-lib";
 
 async function sendDonation(data, hasCode) {
       var axios = require("axios");
-      const res = await axios.post('https://streamlabs.com/api/v1.0/donations?name='+data.name+'&identifier='+data.name+'&amount='+data.amount+'&currency='+data.currency+'&access_token='+hasCode.Item.code);
+      const res = await axios.post('https://streamlabs.com/api/v1.0/donations?name='+data.name+'&identifier='+data.name+'&amount='+data.amount+'&currency='+data.currency+'&access_token='+hasCode.Item.access_token);
       console.log(res);
       return{body:JSON.stringify(res)};
     }
@@ -19,7 +19,7 @@ export const main = handler( async (event, context) => {
     ExpressionAttributeValues:{
       ":wa" : data.walletAddress
     },
-    ProjectionExpression:"code"
+    ProjectionExpression:"access_token"
 
   };
   try{
