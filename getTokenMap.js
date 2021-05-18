693164fb351ce45299bd9cdaa44be33068eb1547
GeekyLumberjack <GeekyLumberjack@gmail.com>
2021-05-18 14:15:13 -0500

    getting token price from blockchain

--- getTokenMap.js
+++ getTokenMap.js
@@ -1,6 +1,14 @@
 import dynamoDb from "./libs/dynamodb-lib";
 import handler from "./libs/handler-lib";
+import {web3Service} from '@unlock-protocol/unlock-js'
 
+async function enrich(map, address){
+  for(var i=0;i<map.length;i++){
+    var getLockPrice = await web3Service.getTokenBalance(Object.values(map[i])[1], address, 100);
+    map[i]['price'] = getLockPrice;
+  }
+  return map
+}
 
 export const main = handler( async (event, context) => {
   const walletAddress = event['queryStringParameters']['walletAddress'];
@@ -18,8 +26,9 @@ export const main = handler( async (event, context) => {
   };
   try{
     const map = await dynamoDb.get(params);
+    const enrichMap = await enrich(map.Item.tokenMap, walletAddress);
     console.log(map);
-    return ({Map: true, Response: map});
+    return ({Map: true, Response: enrichMap});
   } catch (e) {
     console.log(e);
     return { Map: false };
